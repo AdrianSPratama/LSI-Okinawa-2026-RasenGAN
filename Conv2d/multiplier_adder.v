3c16bbcc81c13779b00393677e408e9e91aaee28
AdrianSPratama <16523037@std.stei.itb.ac.id>
2026-01-13 15:09:28 +0700

    tidy up indentation

--- Conv2d/multiplier_adder.v
+++ Conv2d/multiplier_adder.v
@@ -14,37 +14,37 @@ module multiplier_adder # (
     output wire signed [RESULT_WIDTH-1:0] result
 );
 
-// Intermediate multiplication results
-wire signed [RESULT_WIDTH-1:0] p00, p01, p02;
-wire signed [RESULT_WIDTH-1:0] p10, p11, p12;
-wire signed [RESULT_WIDTH-1:0] p20, p21, p22;
+    // Intermediate multiplication results
+    wire signed [RESULT_WIDTH-1:0] p00, p01, p02;
+    wire signed [RESULT_WIDTH-1:0] p10, p11, p12;
+    wire signed [RESULT_WIDTH-1:0] p20, p21, p22;
 
-// Perform multiplications
-assign p00 = x00 * k00;
-assign p01 = x01 * k01;
-assign p02 = x02 * k02;
-assign p10 = x10 * k10;
-assign p11 = x11 * k11;
-assign p12 = x12 * k12;
-assign p20 = x20 * k20;
-assign p21 = x21 * k21;
-assign p22 = x22 * k22;
+    // Perform multiplications
+    assign p00 = x00 * k00;
+    assign p01 = x01 * k01;
+    assign p02 = x02 * k02;
+    assign p10 = x10 * k10;
+    assign p11 = x11 * k11;
+    assign p12 = x12 * k12;
+    assign p20 = x20 * k20;
+    assign p21 = x21 * k21;
+    assign p22 = x22 * k22;
 
-// Adder tree to sum the products
-wire signed [RESULT_WIDTH-1:0] sum0, sum1, sum2, sum3, sum4;
-wire signed [RESULT_WIDTH-1:0] sum01, sum23;
-wire signed [RESULT_WIDTH-1:0] sum0123;
+    // Adder tree to sum the products
+    wire signed [RESULT_WIDTH-1:0] sum0, sum1, sum2, sum3, sum4;
+    wire signed [RESULT_WIDTH-1:0] sum01, sum23;
+    wire signed [RESULT_WIDTH-1:0] sum0123;
 
-assign sum0 = p00 + p01;
-assign sum1 = p02 + p10;
-assign sum2 = p11 + p12;
-assign sum3 = p20 + p21;
-assign sum4 = p22;
+    assign sum0 = p00 + p01;
+    assign sum1 = p02 + p10;
+    assign sum2 = p11 + p12;
+    assign sum3 = p20 + p21;
+    assign sum4 = p22;
 
-assign sum01 = sum0 + sum1;
-assign sum23 = sum2 + sum3;
+    assign sum01 = sum0 + sum1;
+    assign sum23 = sum2 + sum3;
 
-assign sum0123 = sum01 + sum23;
-assign result = sum0123 + sum4;
+    assign sum0123 = sum01 + sum23;
+    assign result = sum0123 + sum4;
 
 endmodule
--- Conv2d/window_reg_3x3.v
+++ Conv2d/window_reg_3x3.v
@@ -1,69 +1,69 @@
 `timescale 1ns / 1ps
 
 module window_reg_3x3 #(
-    parameter DATA_WIDTH = 16
-) (
-    input wire clk,
-    input wire Wr_window, Shift_window, Rst_window,
-    input wire [DATA_WIDTH-1:0] in_row_n, in_row_n_1, in_row_n_2,
-    // Output from every register
-    output wire [DATA_WIDTH-1:0] out_window_00, out_window_01, out_window_02,
-    output wire [DATA_WIDTH-1:0] out_window_10, out_window_11, out_window_12,
-    output wire [DATA_WIDTH-1:0] out_window_20, out_window_21, out_window_22
-);
+        parameter DATA_WIDTH = 16
+    ) (
+        input wire clk,
+        input wire Wr_window, Shift_window, Rst_window,
+        input wire [DATA_WIDTH-1:0] in_row_n, in_row_n_1, in_row_n_2,
+        // Output from every register
+        output wire [DATA_WIDTH-1:0] out_window_00, out_window_01, out_window_02,
+        output wire [DATA_WIDTH-1:0] out_window_10, out_window_11, out_window_12,
+        output wire [DATA_WIDTH-1:0] out_window_20, out_window_21, out_window_22
+    );
 
-integer i, j;
-reg [DATA_WIDTH-1:0] window_reg [0:2][0:2];
-always @(posedge clk) begin
-    if (!Rst_window) begin
-        for (i = 0; i<3; i = i+1) begin
-            for (j = 0; j<3; j = j+1) begin
-                window_reg[i][j] <= 0;
-            end
-        end
-    end
-    else begin
-        if (Wr_window && Shift_window) begin
-            // Write column 0
-            window_reg[0][0] <= in_row_n_2;
-            window_reg[1][0] <= in_row_n_1;
-            window_reg[2][0] <= in_row_n;
-
-            // Shift to right
-            window_reg[0][2] <= window_reg[0][1]; window_reg[0][1] <= window_reg[0][0];
-            window_reg[1][2] <= window_reg[1][1]; window_reg[1][1] <= window_reg[1][0];
-            window_reg[2][2] <= window_reg[2][1]; window_reg[2][1] <= window_reg[2][0];
-        end
-        else if (Wr_window && !Shift_window) begin
-            // Write column 0
-            window_reg[0][0] <= in_row_n_2;
-            window_reg[1][0] <= in_row_n_1;
-            window_reg[2][0] <= in_row_n;
-        end
-        else if (!Wr_window && Shift_window) begin
-            window_reg[0][2] <= window_reg[0][1]; window_reg[0][1] <= window_reg[0][0];
-            window_reg[1][2] <= window_reg[1][1]; window_reg[1][1] <= window_reg[1][0];
-            window_reg[2][2] <= window_reg[2][1]; window_reg[2][1] <= window_reg[2][0];
-        end
-        else begin
+    integer i, j;
+    reg [DATA_WIDTH-1:0] window_reg [0:2][0:2];
+    always @(posedge clk) begin
+        if (!Rst_window) begin
             for (i = 0; i<3; i = i+1) begin
                 for (j = 0; j<3; j = j+1) begin
-                    window_reg[i][j] <= window_reg[i][j];
+                    window_reg[i][j] <= 0;
+                end
+            end
+        end
+        else begin
+            if (Wr_window && Shift_window) begin
+                // Write column 0
+                window_reg[0][0] <= in_row_n_2;
+                window_reg[1][0] <= in_row_n_1;
+                window_reg[2][0] <= in_row_n;
+
+                // Shift to right
+                window_reg[0][2] <= window_reg[0][1]; window_reg[0][1] <= window_reg[0][0];
+                window_reg[1][2] <= window_reg[1][1]; window_reg[1][1] <= window_reg[1][0];
+                window_reg[2][2] <= window_reg[2][1]; window_reg[2][1] <= window_reg[2][0];
+            end
+            else if (Wr_window && !Shift_window) begin
+                // Write column 0
+                window_reg[0][0] <= in_row_n_2;
+                window_reg[1][0] <= in_row_n_1;
+                window_reg[2][0] <= in_row_n;
+            end
+            else if (!Wr_window && Shift_window) begin
+                window_reg[0][2] <= window_reg[0][1]; window_reg[0][1] <= window_reg[0][0];
+                window_reg[1][2] <= window_reg[1][1]; window_reg[1][1] <= window_reg[1][0];
+                window_reg[2][2] <= window_reg[2][1]; window_reg[2][1] <= window_reg[2][0];
+            end
+            else begin
+                for (i = 0; i<3; i = i+1) begin
+                    for (j = 0; j<3; j = j+1) begin
+                        window_reg[i][j] <= window_reg[i][j];
+                    end
                 end
             end
         end
     end
-end
 
-// Assign output
-assign out_window_00 = window_reg[0][0];
-assign out_window_01 = window_reg[0][1];
-assign out_window_02 = window_reg[0][2];
-assign out_window_10 = window_reg[1][0];
-assign out_window_11 = window_reg[1][1];
-assign out_window_12 = window_reg[1][2];
-assign out_window_20 = window_reg[2][0];
-assign out_window_21 = window_reg[2][1];
-assign out_window_22 = window_reg[2][2];
+    // Assign output
+    assign out_window_00 = window_reg[0][0];
+    assign out_window_01 = window_reg[0][1];
+    assign out_window_02 = window_reg[0][2];
+    assign out_window_10 = window_reg[1][0];
+    assign out_window_11 = window_reg[1][1];
+    assign out_window_12 = window_reg[1][2];
+    assign out_window_20 = window_reg[2][0];
+    assign out_window_21 = window_reg[2][1];
+    assign out_window_22 = window_reg[2][2];
 
 endmodule
